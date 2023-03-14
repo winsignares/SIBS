7fa9c16e91066fac8e3d1cc7213a675221060507
Fercho2309 <efibarra67@misena.edu.co>
2023-03-14 16:47:28 -0500

    reparacion de error

--- src/Model/Solicitudes.py
+++ src/Model/Solicitudes.py
@@ -3,9 +3,9 @@ from config.db import db, app, ma
 class Solicitudes(db.Model):
     __tablename__ = "tbSolicitudes"
 
-    id = db.Columm (db.Integer, primary_key = True)
-    fecha_solicitud = db.Columm (db.Datetime)
-    cantidad = db.Columm (db.Integer)
+    id = db.Column (db.Integer, primary_key = True)
+    fecha_solicitud = db.Column (db.Datetime)
+    cantidad = db.Column (db.Integer)
     Id_usu = db.Column(db.Integer, db.ForeignKey('Usuarios.id'))
 
   #  id_usu = db.Columm (db.integer) 
