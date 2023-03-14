ca8ff6477cab0036bb84b535ea1ffe308140da67
camiloski10 <camilocp282@gmail.com>
2023-03-14 16:40:50 -0500

    tabla editoriales

--- src/app.py
+++ src/app.py
@@ -3,6 +3,8 @@ from flask import Flask,  redirect, request, jsonify, json, session, render_temp
 from config.db import db, app, ma
 
 from Model.RolesUsuarios import RolesUsuarios, RolesSchema
+from Model.Editoriales import Editoriales, EditorialesSchema
+
 from Model.Libros import Libros, LibrosSchema
 
 rolesusuario_schema = RolesSchema()
@@ -67,3 +69,55 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000, host='0.0.0.0')
+
+# Datos de la tabla de Editoriales
+@app.route('/Editoriales', methods=['GET'])
+def Editoriales():    
+    returnall = Editoriales.query.all()
+   
+    result_Editoriales = EditorialesSchema.dump(returnall)
+    return jsonify(result_Editoriales)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
