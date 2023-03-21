186d2b418545c833c3604c2d29b558b7b7ced002
JholfrankRC <jholfrank202@gmail.com>
2023-03-21 15:08:10 -0500

    blueprint para usuarios

--- src/api/user.py
+++ src/api/user.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, request, jsonify, json
+from flask import Blueprint, request,jsonify, json
 from common.Toke import *
-
+from config.db import db, app, ma
+from flask import Flask,  redirect, request, jsonify, json, session, render_template
 from Model.Usuarios import Users,UsuariosSchema
 
 routes_user = Blueprint("routes_user", __name__)
@@ -13,6 +14,57 @@ Usuarios_Schema= UsuariosSchema(many=True)
 @routes_user.route("/")
 def index():
     return "index"
+
+@app.route('/Usuarios', methods=['GET'])
+def usuarios():    
+    returnall = Users.query.all()
+   
+    resultado_usuarios = Usuarios_Schema.dump(returnall)
+    return jsonify(resultado_usuarios)
+
+#crud de usuarios
+@app.route('/eliminar_Users/<id>', methods=['GET'] )
+def eliminar_users(id):
+    full_name = Users.query.get(id)
+    Email = Users.query.get(id)
+    telefono = Users.query.get(id)
+    especialidad = Users.query.get(id)
+    jornada = Users.query.get(id)
+    direccion = Users.query.get(id)
+    db.session.delete(full_name,Email,telefono,especialidad,jornada,direccion)
+    db.session.commit()
+    return jsonify(UsuariosSchema.dump(full_name,Email,telefono,especialidad,jornada,direccion)) 
+
+@app.route('/actualizarUsers', methods=['POST'] )
+def actualizar_users():
+    id = request.json['id']
+    full_name = request.json['full_name']
+    Email= request.json['Email']
+    telefono= request.json['telefono']
+    especialidad= request.json['especialidad']
+    jornada= request.json['jornada']
+    direccion= request.json['direccion']
+    users= Users.query.get(id)
+    users.full_name = full_name
+    users.Email= Email
+    users.telefono = telefono
+    users.especialidad= especialidad
+    users.jornada = jornada
+    users.direccion= direccion
+    db.session.commit()
+    return redirect('/Usuarios')
+
+@app.route('/save_Users', methods=['POST'] )
+def guardar_Users():
+    full_name = request.json['full_name']
+    Email = request.json['Email']
+    telefono = request.json['telefono']
+    especialidad = request.json['especialidad']
+    jornada = request.json['jornada']
+    direccion= request.json['direccion']
+    print(full_name,Email,telefono,especialidad,jornada,direccion)
+    new_Users = Users(full_name,Email,telefono,especialidad,jornada,direccion)
+    db.session.add(new_Users)
 
 @routes_user.route('/obtenerToken', methods=['GET'])
 def obtenertoken():
