d6c8081d2d663a7a04eb1e59803bcd0ec5f0b83c
JholfrankRC <jholfrank202@gmail.com>
2023-03-23 14:05:08 -0500

    campo de contraseña en usuarios

--- src/Model/Usuarios.py
+++ src/Model/Usuarios.py
@@ -6,14 +6,16 @@ class Users(db.Model):
     
     id = db.Column(db.Integer,primary_key=True)
     full_name= db.Column(db.String(200))
+    password= db.column(db.String(200))
     Email= db.Column(db.String(200))
     telefono= db.Column(db.Integer)
     especialidad= db.Column(db.String(200))
     jornada= db.Column(db.String(200))
     direccion= db.Column(db.String(200))
     
-    def __init__(self,full_name,Email,telefono,especialidad,jornada,direccion):
+    def __init__(self,full_name,password,Email,telefono,especialidad,jornada,direccion):
        self.full_name= full_name
+       self.password= password
        self.Email= Email
        self.telefono= telefono
        self.especialidad= especialidad
@@ -25,7 +27,7 @@ class Users(db.Model):
 
 class UsuariosSchema(ma.Schema):
     class Meta:
-        fields = ('id','full_name','Email','telefono','especialidad','jornada','direccion')
+        fields = ('id','full_name','password','Email','telefono','especialidad','jornada','direccion')
         
     
     
