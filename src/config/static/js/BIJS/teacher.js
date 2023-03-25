efe7e35f8abaf7e0066101882f4da90e17ad02ff
julsk12 <jpsierra316@misena.edu.co>
2023-03-25 00:20:37 -0500

    teacher

--- src/config/static/js/BIJS/teacher.js
+++ src/config/static/js/BIJS/teacher.js
@@ -10,15 +10,13 @@ function GuardarInstructor() {
     const passintruc = document.getElementById('passinstructor');
     const passinstruc = document.getElementById('passinstructor2');
 
-    //passadmin.value === passadmin2.value ? alert(`Estos son los datos del administrador ${nameadmin.value} ${emailadmin.value}${usernameadmin.value} ${passadmin.value}`): alert('los campos son diferentes');
     axios.post('guardaradmin', {
         fullname: nameinstructor.value,
         Email: emailadmin.value,
         Telefono: telinstructor.value,
         Especialidad: espeinstructor.value,
         Jornada: turnoinstruc.value        
-        //orders: [1, 2, 3],
-        //photo: document.querySelector('#fileInput').files
+       
     }, {
         headers: {
         'Content-Type': 'multipart/form-data'
