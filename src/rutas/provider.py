b1b7c5e30ee22f5e16eea601b36e995f173dd1b2
wilches018 <brandowilches2014@gmail.com>
2023-03-28 15:15:49 -0500

    provider

---
+++ src/rutas/provider.py
@@ -0,0 +1,23 @@
+from config.db import db, app, ma
+from flask import Blueprint, Flask,  redirect, request, jsonify, json, session, render_template
+from Model.Proveedores import Proveedores
+
+routes_provider = Blueprint("routes_provider", __name__)
+
+@routes_provider.route('/indexprovider', methods=['GET'] )
+def indexprovider():
+    
+    return render_template('/main/provider.html')
+
+@routes_provider.route('/saveproveedor', methods=['POST'] )
+def guardarprovedo():
+    Nombre_proveedor = request.json['Nombre_proveedor']
+    Telefono = request.json['Telefono']
+    Direccion = request.json['Direccion']
+    Descripcion = request.json['Descripcion']
+    
+    print(Nombre_proveedor,Telefono,Direccion,Descripcion)
+    new_provider = Proveedores(Nombre_proveedor,Telefono,Direccion,Descripcion)
+    db.session.add(new_provider)
+    db.session.commit()
+    return redirect('/proveedores')
