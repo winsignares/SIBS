367f06ee86c0486a3432f3dccc3887edcfe03c86
Fercho2309 <efibarra67@misena.edu.co>
2023-03-29 15:10:58 -0500

    SS

--- src/config/static/js/BIJS/listsesion.js
+++ src/config/static/js/BIJS/listsesion.js
@@ -29,19 +29,3 @@ function listcategory(){
 
 }
 
-
-
-/*  axios.get('/viewlistcategory',{
-        .then(function(response){
-            console.log(response)
-        }).catch(function(error){
-            console.log(error)
-    })
-        .then(function(){
-    })
-    })
-
-axios.get('/viewlistcategory').then((response)=>{
-    setPost(response.data)
-}
-)*/
--- src/rutas/listsesion.py
+++ src/rutas/listsesion.py
@@ -1,5 +1,7 @@
 from config.db import db, app, ma
 from flask import Blueprint, Flask,  redirect, request, jsonify, json, session, render_template
+from model.section import section , sectionSchema 
+
 
 routes_listsesion = Blueprint("routes_listsesion", __name__)
 
@@ -8,3 +10,11 @@ routes_listsesion = Blueprint("routes_listsesion", __name__)
 def indexlistsesion():
     
     return render_template('/main/listsection.html')
+
+
+
+@routes_listsesion.route('/viewlistcategory', methods=['GET'])
+def viewlistcategory():
+        returnall = section.query.all()
+        result_section = sectionSchema.dump(returnall)
+        return result_section
