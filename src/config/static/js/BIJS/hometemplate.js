714047758ac4f4c972a2759093fb447d73ee2031
Santiago L <santiagolozman@gmail.com>
2023-03-30 11:22:06 -0500

    Agrego un href a administracion de libros la etiqueta h

--- src/config/static/js/BIJS/hometemplate.js
+++ src/config/static/js/BIJS/hometemplate.js
@@ -112,20 +112,7 @@ var enlaceVolver = document.getElementById("volver-atras");
 enlaceVolver.addEventListener("click", function(event) {
   event.preventDefault(); // Evitar que se actualice la página
   ocultarContenido("contenidoAdmin"); // Ocultar el contenido
-
-  volverAtras.removeAttribute("disabled");
-
 });
-function volverAtras() {
-  // Ocultar el contenido del iframe y mostrar los mosaicos nuevamente
-  contenidoAdmin.style.display = "none";
-  for (var i = 0; i < tiles.length; i++) {
-    tiles[i].style.display = "block";
-  }
-
-  // Desactivar el enlace "volver-atras" nuevamente
-  volverAtras.setAttribute("disabled", "disabled");
-}
 
 
 
