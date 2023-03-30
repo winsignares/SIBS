73be6d1accd1a37b2171e0962285f2117264c3c2
julsk12 <jpsierra316@misena.edu.co>
2023-03-29 20:45:40 -0500

    jdmd

--- src/rutas/teacher.py
+++ src/rutas/teacher.py
@@ -10,19 +10,18 @@ def indexteacher():
     return render_template('/main/teacher.html')
 
 @routes_teacher.route('/saveinstructor', methods=['POST'] )
-def guardarinstru():
-    								
+def guardarinstru():   								
 
     full_name = request.form['full_name']
-    password = request.form['password']
     Email = request.form['Email']
     telefono = request.form['telefono']
     especialidad = request.form['especialidad']
     jornada = request.form['jornada']
     id_roles = request.form['id_roles']
     cedula = request.form['cedula']
+    password = request.form['password']
     print(full_name,Email,telefono,especialidad,jornada)
-    new_Users = Users(full_name, password, Email,telefono,especialidad,jornada, id_roles, cedula)
+    new_Users = Users(full_name, Email,telefono,especialidad,jornada, id_roles, cedula, password)
     db.session.add(new_Users)
     db.session.commit()
-    return redirect('/Usuarios')
+    return full_name
