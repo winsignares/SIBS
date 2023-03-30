1e7cc22ef374f513e3303c6e5c9c28c9f0979898
camiloski10 <camilocp282@gmail.com>
2023-03-30 14:55:17 -0500

    Arreglo categoria

--- src/rutas/Categoria.py
+++ src/rutas/Categoria.py
@@ -2,15 +2,15 @@ from config.db import db, app, ma
 from flask import Blueprint, Flask,  redirect, request, jsonify, json, session, render_template
 from Model.Categorias import *
 
-routes_category = Blueprint("routes_categ", __name__)
+routes_categorias = Blueprint("routes_categ", __name__)
 
 
-@routes_category.route('/indexCategoria', methods=['GET'] )
+@routes_categorias.route('/indexCategoria', methods=['GET'] )
 def indexCategoria():
     
     return render_template('/main/Categoria.html')
 
-@routes_category.route('/guardarcategoria',methods=['POST'])
+@routes_categorias.route('/guardarcategoria',methods=['POST'])
 def savecategoria():
    
  #request.form['title']
