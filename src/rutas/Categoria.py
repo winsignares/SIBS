92d911aab69049d9a3f58c4e714013f9db0f39ad
camiloski10 <camilocp282@gmail.com>
2023-03-30 14:52:56 -0500

    Agrego para guardar desde js CATEGORIA

--- src/rutas/Categoria.py
+++ src/rutas/Categoria.py
@@ -1,11 +1,11 @@
 from config.db import db, app, ma
 from flask import Blueprint, Flask,  redirect, request, jsonify, json, session, render_template
-from Model.Categorias import Categorias, CategoriasSchema
+from Model.Categorias import *
 
 routes_category = Blueprint("routes_categ", __name__)
 
 
-@routes_category.route('/indexcategoria', methods=['GET'] )
+@routes_category.route('/indexCategoria', methods=['GET'] )
 def indexCategoria():
     
     return render_template('/main/Categoria.html')
