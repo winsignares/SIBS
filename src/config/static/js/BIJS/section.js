3aa1c804789291ec082857e3fda6750ba38a8db8
JohnYL47 <jylaguna6@misena.edu.co>
2023-03-30 16:10:12 -0500

    modified section.js -inn

--- src/config/static/js/BIJS/section.js
+++ src/config/static/js/BIJS/section.js
@@ -3,11 +3,6 @@ function addSection() {
     const OpYear = document.getElementById('getOptionYear').value;
     const OpEspecialidad = document.getElementById('getOptionEspecialidad').value;
     const OpSeccion = document.getElementById('getOptionSeccion').value;
-<<<<<<< HEAD
-    //passadmin.value === passadmin2.value ? alert(`Estos son los datos del administrador ${Inputname.value} ${emailadmin.value}${userInputname.value} ${passadmin.value}`): alert('los campos son diferentes');
-=======
-
->>>>>>> main
     axios.post('guardarsection', {
             year: OpYear,
             especialidad: OpEspecialidad,
