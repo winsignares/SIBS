ab4842f679218cd348827068b71e031415ca12b9
Santiago L <santiagolozman@gmail.com>
2023-03-30 01:10:29 -0500

    Agrego responsive boton

--- src/config/static/js/BIJS/hometemplate.js
+++ src/config/static/js/BIJS/hometemplate.js
@@ -80,41 +80,6 @@ function ocultarContenido(id) {
   }
 }
 
-//RESPONSIVE BUTTOM
-
-if (window.screen.width < 768) {
- 
-  var list;
-  list = document.querySelectorAll(".boton-volver");
-  for (var i = 0; i < list.length; ++i) {
-      list[i].classList.add('btn-chico');
-  }
-
-  console.log('Ventana Menor que 768px');
-
-}
-else if (window.screen.width < 900) {
-
-  var list;
-  list = document.querySelectorAll(".boton-volver");
-  for (var i = 0; i < list.length; ++i) {
-      list[i].classList.add('btn-mediano');
-  }
-
-  console.log('Ventana Menor que 900px');
-
-}
-else if (window.screen.width < 1200) {
-
-  var list;
-  list = document.querySelectorAll(".boton-volver");
-  for (var i = 0; i < list.length; ++i) {
-      list[i].classList.add('btn-grande');
-  }
-
-  console.log('Ventana Menor que 1200px');
-  
-}
 
 //Este es un codigo llamativo
 /*function mostrarContenido(id) {
