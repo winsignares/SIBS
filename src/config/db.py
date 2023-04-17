e8f23f610ca9145e3a2b38db24e94ba04fcde8e9
julsk12 <jpsierra316@misena.edu.co>
2023-04-17 11:24:37 -0500

    Docker

--- src/config/db.py
+++ src/config/db.py
@@ -3,9 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
-url_con = 'mysql+pymysql://root@localhost/adso'
-app.config['SQLALCHEMY_DATABASE_URI'] = url_con
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root@localhost/adso'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+
 
 app.secret_key = "adso3"
 
